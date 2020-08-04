2e0443e2f2d7af1d6ac6378cbfa1089743f1227c
Justin <Justin@Peabody.local>
2020-08-03 23:26:23 -0700

    leetcode binary search answer

--- algos/combos/algo_sorted-arr-to-bst.js
+++ algos/combos/algo_sorted-arr-to-bst.js
@@ -17,25 +17,47 @@ function TreeNode(val, left, right) {
 // find mid recursively
 // [-10,-3,0,5,7,9,12,25,82]
 
+// var sortedArrayToBST = function(nums) {
+//     let lo = 0                            // 0
+//     let hi = nums.length - 1              // 8
+//     let root = new TreeNode(null)
+
+//     return arrToBSTRecur(lo, hi, root, nums, [])
+// }
+
+// function arrToBSTRecur(lo, hi, node, arr, res) {
+//     mid = Math.floor((lo + hi) / 2)      
+//     if (lo > hi || res.includes(mid)) return node
+//     node = new TreeNode(arr[mid])
+//     console.log(`mid = ${mid}`)
+//     res.push(mid)
+
+//     return (
+//         arrToBSTRecur(lo, mid - 1, node.left, arr, res), // left side
+//         arrToBSTRecur(mid + 1, hi, node.right, arr, res) // right side
+//     )
+// }
+
+// let test = sortedArrayToBST([-10,-3,0,5,7,9,12,25,82])
+// console.log(test)
+
 var sortedArrayToBST = function(nums) {
     let lo = 0                            // 0
     let hi = nums.length - 1              // 8
-    let root = new TreeNode(null)
 
-    return arrToBSTRecur(lo, hi, root, nums, [])
+    return arrToBSTRecur(lo, hi, nums)
 }
 
-function arrToBSTRecur(lo, hi, node, arr, res) {
-    mid = Math.floor((lo + hi) / 2)      
-    if (lo > hi || res.includes(mid)) return node
-    node = new TreeNode(arr[mid])
-    console.log(`mid = ${mid}`)
-    res.push(mid)
+function arrToBSTRecur(lo, hi, arr) {
+    if (lo > hi) return null
+    let mid = Math.floor((lo + hi) / 2)
 
-    return (
-        arrToBSTRecur(lo, mid - 1, node.left, arr, res), // left side
-        arrToBSTRecur(mid + 1, hi, node.right, arr, res) // right side
-    )
+    let node = new TreeNode(arr[mid])
+
+    node.left = arrToBSTRecur(lo, mid - 1, arr)
+    node.right = arrToBSTRecur(mid + 1, hi, arr)
+
+    return node
 }
 
 let test = sortedArrayToBST([-10,-3,0,5,7,9,12,25,82])
