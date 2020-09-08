a53fa920b2a4977a24856d30b3bd1273498fbafb
Justin <justin@jjohnson.io>
2020-09-08 16:15:59 -0700

    add group anagrams

---
+++ algos/strength-algos/group-anagrams.js
@@ -0,0 +1,24 @@
+function groupAnagrams(words) {
+    let res = []
+    let hash = {}
+    let alpha = words.map((word) => word.split('').sort().join(''))
+
+    for (i in words) {
+        if (hash[alpha[i]]) {
+            hash[alpha[i]].push(i)
+        } else {
+            hash[alpha[i]] = [i]
+        }
+    }
+
+    for (item in hash) {
+        let indexes = [...hash[item]]
+        res.push(indexes.map((i) => words[i]))
+    }
+
+    return res
+  }
+
+let words = ["yo", "act", "flop", "tac", "foo", "cat", "oy", "olfp"]
+ // alpha = ["oy", "act", "flop", "act", "foo", "act", "oy", "flop"]
+console.log(groupAnagrams(words))
