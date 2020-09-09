3304ad1b068df95cee97adee8014aebeb285c317
Justin Johnson <justin@jjohnson.io>
2020-09-08 17:42:05 -0700

    optimized group-anagrams

--- algos/strength-algos/group-anagrams.js
+++ algos/strength-algos/group-anagrams.js
@@ -1,24 +1,42 @@
-function groupAnagrams(words) {
-    let res = []
-    let hash = {}
-    let alpha = words.map((word) => word.split('').sort().join(''))
+// function groupAnagrams(words) {
+//     let res = []
+//     let hash = {}
+//     let alpha = words.map((word) => word.split('').sort().join(''))
 
-    for (i in words) {
+//     for (i in words) {
+//         if (hash[alpha[i]]) {
+//             hash[alpha[i]].push(i)
+//         } else {
+//             hash[alpha[i]] = [i]
+//         }
+//     }
+
+//     for (item in hash) {
+//         let indexes = [...hash[item]]
+//         res.push(indexes.map((i) => words[i]))
+//     }
+
+//     return res
+//   }
+
+// let words = ["yo", "act", "flop", "tac", "foo", "cat", "oy", "olfp"]
+//  // alpha = ["oy", "act", "flop", "act", "foo", "act", "oy", "flop"]
+// console.log(groupAnagrams(words))
+
+function groupAnagrams(words) {
+    let hash = {} // hash of indexes
+    let alpha = words.map((word) => word.split('').sort().join(''))
+    
+    for (let i in words) {
         if (hash[alpha[i]]) {
-            hash[alpha[i]].push(i)
+            hash[alpha[i]].push(words[i])
         } else {
-            hash[alpha[i]] = [i]
+            hash[alpha[i]] = [words[i]]
         }
     }
+    
+    return Object.values(hash)
+}
 
-    for (item in hash) {
-        let indexes = [...hash[item]]
-        res.push(indexes.map((i) => words[i]))
-    }
-
-    return res
-  }
-
-let words = ["yo", "act", "flop", "tac", "foo", "cat", "oy", "olfp"]
- // alpha = ["oy", "act", "flop", "act", "foo", "act", "oy", "flop"]
+const words = ["yo", "act", "flop", "tac", "foo", "cat", "oy", "olfp"]
 console.log(groupAnagrams(words))
