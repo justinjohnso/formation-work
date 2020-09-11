4fc270d01c0c431c515e6b00e7235edfe4c2b316
Justin <Justin@Wheatley.local>
2020-09-10 19:06:47 -0700

    a bit of cleanup

--- live-coding-pair-sessions/pokemon_9-10/src/App.js
+++ live-coding-pair-sessions/pokemon_9-10/src/App.js
@@ -1,60 +1,96 @@
 import React, { useState, useEffect } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { List, ListItem, Container, Button } from "@material-ui/core/";
-// import Pokedex from "pokeapi-js-wrapper"
+// import { makeStyles } from "@material-ui/core/styles";
+import {
+  List,
+  ListItem,
+  ListItemText,
+  Container,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogActions,
+  Typography,
+  Box
+} from "@material-ui/core/";
 
-const useStyles = makeStyles((theme) => ({}));
+// const useStyles = makeStyles((theme) => ({}));
 
 const Pokedex = require("pokeapi-js-wrapper");
 const P = new Pokedex.Pokedex();
 
 const ListPokemon = () => {
+  const [open, setOpen] = useState(false);
   const [offset, setOffset] = useState(0);
   const [currPokemon, setCurrPokemon] = useState();
-  const [pokeList, setPokeList] = useState([
-    { name: "charmander" },
-    { name: "pikachu" },
-    { name: "squirtle" }
-  ]);
+  const [pokeList, setPokeList] = useState([]);
 
-  useEffect(() => {
-    fetchAllPokemon();
-  });
+  useEffect(() => { fetchAllPokemon(); });
+
+  const handleOpen = (name) => {
+    P.getPokemonByName(name).then((res) => setCurrPokemon(res));
+    console.log(currPokemon);
+    setOpen(true);
+    console.log(open)
+  }
+
+  const handleClose = () => {
+    setOpen(false);
+    console.log(open)
+  }
 
   const fetchAllPokemon = () =>
-    P.getPokemonsList({ limit: 5, offset }).then(function (response) {
+    P.getPokemonsList({ limit: 151}).then(function (response) {
       setPokeList(response.results);
     });
 
-  const handleClick = (name) => {
-    P.getPokemonByName(name).then((res) => setCurrPokemon(res));
+  const PokemonDialog = () => {
+    return (
+      <>
+        <Dialog onClose={handleClose} open={open}>
+          {/* <DialogTitle>{currPokemon.name}</DialogTitle> */}
+          {/* <List>
+            <ListItem>
+              <Typography>HP:{}</Typography>
+              <Typography>Defense:{}</Typography>
+              <Typography>Attack:{}</Typography>
+              <Typography>Speed:{}</Typography>
+              <Typography>Type:{}</Typography>
+              <Typography>Weight:{}</Typography>
+            </ListItem>
+          </List> */}
+          <DialogActions>
+           <Button onClick={handleClose}>Close</Button>
+          </DialogActions>
+        </Dialog>
+      </>
+    );
   };
-
   return (
     <>
       <List>
-        {pokeList.map((pokemon) => (
+        {pokeList.slice(offset, offset + 5).map((pokemon, index) => (
           <ListItem key={pokemon.name}>
-            <Button onClick={() => handleClick(pokemon.name)}>
-              {pokemon.name}
-            </Button>
+            <Box fullWidth onClick={() => handleOpen(pokemon.name)}>
+              <Box flexGrow={1}><Typography align='left'>{pokemon.name}</Typography></Box>
+              <Box><Typography>{(index + 1 + offset).toString().padStart(3, 0)}</Typography></Box>
+            </Box>
           </ListItem>
         ))}
       </List>
-      <Button onClick={() => setOffset(offset > 0 ? offset - 5 : offset)}>
-        Prev
-      </Button>
-      <Button onClick={() => setOffset(offset + 5)}>Next</Button>
-      {/* <div>{currPokemon}</div> */}
+      <Button variant="outlined" onClick={() => setOffset(offset > 0 ? offset - 5 : offset)}>Prev</Button>
+      <Button variant="outlined" onClick={() => setOffset(offset < 150 ? offset + 5 : offset)}>Next</Button>
+      <PokemonDialog />
     </>
   );
 };
 
 export default function App() {
-  const classes = useStyles();
+  // const classes = useStyles();
   return (
-    <Container>
-      <ListPokemon />
-    </Container>
+    <>
+      <Container maxWidth='sm'>
+        <ListPokemon />
+      </Container>
+    </>
   );
 }
