0c23c4d9775b95b88851910ddb03bdc9f261f77b
Justin Johnson <justin@jjohnson.io>
2020-10-06 13:37:42 -0700

    add notion live coding

---
+++ coding-challenges/notion/notion-live/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Notion Live Coding</h1>
+      <p>Lets do thiiiiis!</p>
+    </div>
+  );
+}
+
+export default App;
